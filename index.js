b4be79b9492745c59d2c5be67825d1091bb61c28
Evan Plaice <evanplaice@gmail.com>
2021-05-11 12:06:31 -0600

    Fix Build Script

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-var __defProp = Object.defineProperty;
-var __markAsModule = (target) => __defProp(target, "__esModule", {value: true});
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, {get: all[name], enumerable: true});
-};
-
-// src/wc-demo.js
-__markAsModule(exports);
-__export(exports, {
-  WCDemo: () => WCDemo
-});
-
 // node_modules/prism-es6/prism.js
 var _self = {};
 var Prism = function() {
@@ -913,3 +900,6 @@ var WCDemo = class extends HTMLElement {
   }
 };
 customElements.define("wc-demo", WCDemo);
+export {
+  WCDemo
+};
